a68b2166da8ae56815d55e0808d3d2cec72a2d99
Jonas Depoix <jdepoix@seibert-media.net>
2016-01-14 15:24:12 +0100

    scraper implemented

---
+++ scraper.js
@@ -0,0 +1,41 @@
+(function($) {
+	$.extend({
+		scrapeHAPDocs: function(url, callback) {
+			var docData = [];
+			var virtualKeywordsDom = null;
+
+			loadDocPage();
+
+			function loadDocPage() {
+				$.ajax({
+					url: url,
+					success: initVirtualDom
+				});
+			}
+
+			function initVirtualDom(htmlString) {
+				virtualKeywordsDom = $($.parseHTML(htmlString)[13]).find(".keyword");
+				parseDocData();
+			}
+
+			function parseDocData() {
+				virtualKeywordsDom.each(parseKeyword);
+				callback(docData);
+			}
+
+			function parseKeyword() {
+				var anchor = $(this).find("b a:not(.anchor)")[0];
+				var keyword = $(anchor).text();
+				var params = $(this).text().substring(keyword.length);
+
+				var command = {
+					"keyword": keyword,
+					"params": params,
+					"id": $(anchor).attr("href")
+				};
+
+				docData.push(command);
+			}
+		}
+	});
+})(jQuery);
